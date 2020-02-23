9f8ed77a186d75bbcb5e3cd3c1cca7092975d6ca
matsuuraaoi <hatimitu999@outlook.jp>
2020-02-23 20:14:46 +0900

    GoForwardUnityCHan 課題
    衝突時に効果音をつける

---
+++ Assets/CubeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	//キューブの移動速度
+	private float speed = -12;
+
+	//消滅位置
+	private float deadLine = -10;
+
+	//課題
+	private GameObject unitychan;
+	private GameObject othercube;
+	private GameObject groundss;
+	// Use this for initialization
+	void Start () {
+		this.unitychan = GameObject.Find ("UnityChan2D");
+		this.othercube = GameObject.Find ("CubePrefab");
+		this.groundss = GameObject.Find ("ground");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (this.speed * Time.deltaTime, 0, 0);
+		
+		//画面外に出たら破棄する
+		if (transform.position.x < this.deadLine) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject == unitychan) {
+			GetComponent<AudioSource> ().volume = 0;
+		} else if (other.gameObject == othercube || other.gameObject == groundss) {
+			GetComponent<AudioSource> ().volume = 1;
+		} else {
+			GetComponent<AudioSource> ().volume = 0;
+		}
+	}
+}
---
+++ Assets/CubeGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGenerator : MonoBehaviour {
+	//課題
+	Animator animator;
+	//キューブのPrefab
+	public GameObject cubePrefab;
+
+	//時間計測用の変数
+	private float delta = 0;
+
+	//キューブの生成間隔
+	private float span = 1.0f;
+
+	//キューブの生成位置：X座標
+	private float genPosX = 12;
+
+	//キューブの生成位置オフセット
+	private float offsetY = 0.3f;
+
+	//キューブの縦方向の間隔
+	private float spaceY = 6.9f;
+
+	//キューブの生成位置オフセット
+	private float offsetX = 0.5f;
+	//キューブの横方向の間隔
+	private float spaceX = 0.4f;
+
+	//キューブの生成個数の上限
+	private int maxBlockNum = 4;
+	 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.delta += Time.deltaTime;
+		//GetComponent<AudioSource> ().volume = 0;
+		//span秒以上の時間が経過したかを調べる
+		if (this.delta > this.span) {
+			this.delta = 0;
+			//生成するキューブ数をランダムに決める
+			int n = Random.Range (1, maxBlockNum+1);
+
+			//指定した数だけキューブを生成する
+			for (int i = 0; i < n; i++) {
+				//キューブの生成
+				GameObject go = Instantiate (cubePrefab) as GameObject;
+				go.transform.position = new Vector2 (this.genPosX, this.offsetY + i * this.spaceY);
+				//課題
+				Vector2 goposi = go.transform.position;
+				bool onGround = (goposi.x == this.genPosX) ? true : false;
+				//this.animator.SetBool ("onGround", onGround);
+				GetComponent<AudioSource> ().volume = (onGround) ? 1 : 0;
+			}
+			//次のキューブまでの生成時間を決める
+			this.span = this.offsetX + this.spaceX * n;
+		}
+
+	}
+		
+}
